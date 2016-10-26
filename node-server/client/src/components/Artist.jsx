5035b3a690785a9cca0d832e04047ddc310807f3
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-26 14:35:48 +0100

    catalog raw data finished

--- node-server/client/src/components/Artist.jsx
+++ node-server/client/src/components/Artist.jsx
@@ -1,11 +1,11 @@
 const React = require('react')
-// const Album = require('album')
+const Album = require('./Album')
 
 let Artist = (props)=>{
   console.log(props)
   let albums=props.artistData.albums.map((album)=>{
     console.log(album)
-    return <Album albumInfo={album}/>
+    return <Album key={album.id} albumInfo={album}/>
   })
   console.log(albums)
 
@@ -13,9 +13,10 @@ let Artist = (props)=>{
 
   return (
     <div>
-    <h4> Name: {props.artistData.name}</h4>
-    <p> Genre: {props.artistData.genre}</p>
-    <div className="album-components-container">{albums}</div>
+      <h4> Name: {props.artistData.name}</h4>
+      <p> Genre: {props.artistData.genre}</p>
+      <div className="album-components-container">{albums}</div>
+      <hr></hr>
     </div>)
 }
 
