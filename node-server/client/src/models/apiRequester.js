22b5f65a5b806221e4827f049cf046c6ce0c1d18
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-26 14:48:33 +0100

    added post request to api requester

--- node-server/client/src/models/apiRequester.js
+++ node-server/client/src/models/apiRequester.js
@@ -5,6 +5,14 @@ const apiRequester = {
     request.open("GET", url);
     request.onload = callback;
     request.send();
+  },
+  post: function(url, object){
+    jsonString = JSON.stringyfy(object)
+    let request = new XMLHttpRequest();
+    request.open("POST", url);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(jsonString)
   }
+  
 }
 module.exports = apiRequester;
