a72c536b52a17c531dc3820e6986a0acd53a6c96
srobertson90 <stuart.robertson.applications@gmail.com>
2016-10-26 10:56:01 +0100

    added api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists do
-    resources :albums do
-      resources :stocks
+  scope path: "api" do
+    resources :artists do
+      resources :albums do
+        resources :stocks
+      end
     end
   end
 end
